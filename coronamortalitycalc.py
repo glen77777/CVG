21bc43bdded1a80da743c0a30e71785679b6fc89
bobbi g <39740428+glen77777@users.noreply.github.com>
2020-02-19 06:57:26 -0500

    Update coronamortalitycalc.py

--- coronamortalitycalc.py
+++ coronamortalitycalc.py
@@ -1,5 +1,9 @@
 number = int(input("known infected: "))
-mort = 608 / 14982
-print(mort)  
-deaths = number * mort
-print(round(deaths, 2)) 
+realmort = 608 / 14982
+print('Real Mortality rate', realmort)  
+realdeaths = number * realmort
+print('real death count' ,round(realdeaths, 2))
+fakemort = 2012 / 75201
+print('Fake Mortality rate', fakemort)  
+fakedeaths = number * fakemort
+print('fake death count' ,round(fakedeaths, 2)) 
